2480ab036ff71f2a0bbcd827d77d944561c9532d
BeomyeonAndrewKim <beom911@gmail.com>
2018-01-26 14:53:14 +0900

    port change for https

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
-var port = 3000;
+var port = 80;
 
 var app = express();
 
